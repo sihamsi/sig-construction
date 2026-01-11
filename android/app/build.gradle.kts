0cf6f483472988783bdb48b1e0df948940b68e46
Meryem Benchelh <meryembenchelh@gmail.com>
2026-01-11 13:48:02 +0100

    Fix android gradle configuration

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,10 @@ plugins {
 android {
     namespace = "com.example.sig_construction"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
+
+
+
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
